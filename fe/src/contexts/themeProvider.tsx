75374471290ce7cd7da66a6abbb47da490de49b6
YouSooA <kinsk2839@naver.com>
2023-08-30 16:09:58 +0900

    feat: theme 관련 context 생성 (#3)

--- fe/src/contexts/themeProvider.tsx
+++ fe/src/contexts/themeProvider.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 
-import { ThemeContext } from 'styled-components';
+import { ThemeProvider as StyledProvider } from 'styled-components';
 
 import { fonts, lightColors, darkColors } from '@styles/theme';
 
 interface Props {
   children: React.ReactNode;
 }
+
+type ThemeContextType = {
+  themeMode: 'light' | 'dark';
+  handleThemeChange: () => void;
+};
+
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
+const useTheme = () => {
+  const value = useContext(ThemeContext);
+
+  if (!value) {
+    throw new Error('useTheme should be used within ThemeProvider');
+  }
+
+  return value;
+};
 
 const ThemeProvider = ({ children }: Props) => {
   const [isDark, setIsDark] = useState<boolean>(false);
@@ -20,7 +37,11 @@ const ThemeProvider = ({ children }: Props) => {
 
   const theme = { fonts, colors: isDark ? darkColors : lightColors };
 
-  return <ThemeContext.Provider value={{ theme, handleThemeChange }}>{children}</ThemeContext.Provider>;
+  return (
+    <ThemeContext.Provider value={{ themeMode: isDark ? 'dark' : 'light', handleThemeChange }}>
+      <StyledProvider theme={theme}>{children}</StyledProvider>
+    </ThemeContext.Provider>
+  );
 };
 
-export default ThemeProvider;
+export { useTheme, ThemeProvider };
